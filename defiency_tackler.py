b2cf01202d0baa396af9e33afef66ed58a460a9f
Franz Tapia Chaca <75755705+franztapiac@users.noreply.github.com>
2023-10-20 19:59:46 +0200

    created database reader

---
+++ defiency_tackler.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+from pandas import read_csv
+
+
+def read_database(db_path):
+    db = read_csv(filepath_or_buffer=db_path)
+    return db
+
+
+def tackle_deficiency(def_nutrient: str, db_path: Path):
+    """
+    Run the whole deficiency tackler program. Given the deficiency, find the foods that one can have the nutrient
+    or compound that one needs to tackle this deficiency. Sort the foods by concentration of the nutrient.
+    :param def_nutrient: The nutrient that the user is deficient in.
+    :type def_nutrient: str.
+    :param db_path: The path to the food content database.
+    :type db_path: Path.
+    """
+
+    # Search database for all foods with def_nutrient (get the list that the Excel search gives you)
+
+    # From all hits
+        # separate into different compounds / nutrients by their source_id or orig_source_id
+        # For each (orig_)source_id
+            # list out the food they are found in together with the content
+
+    pass
+
+
+if __name__ == '__main__':
+    database_path = Path(r"C:\Users\franz\Documents\work\projects\deficiency_tackler\data\foodb_2020_04_07_csv\foodb_2020_04_07_csv\Content.csv")
+    deficient_nutrient = 'luteolin'
+    tackle_deficiency(def_nutrient=deficient_nutrient, db_path=database_path)
+
+
+
