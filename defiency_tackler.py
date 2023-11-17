3d547aff0a5f0abafa39b59696b87e7d7cbe1fb4
Franz Tapia Chaca <75755705+franztapiac@users.noreply.github.com>
2023-11-17 18:57:55 +0100

    exported FooDB Content into 12 chunks

--- defiency_tackler.py
+++ defiency_tackler.py
@@ -6,11 +6,25 @@ current_file_str_path = os.path.abspath(__file__)
 repo_root_str_path = Repo(current_file_str_path, search_parent_directories=True).git.rev_parse("--show-toplevel")
 sys.path.append(repo_root_str_path)
 import csv
+import json
 import io
 import numpy as np
 import pandas as pd
 from time import time
 import re
+
+def read_and_write_json(input_file, output_file, num_lines=5):
+    # Read data from the input JSON file
+    with open(input_file, 'r') as f:
+        data = json.load(f)
+
+    # Take the first 'num_lines' lines
+    subset_data = data[:num_lines]
+
+    # Write the subset data to the output JSON file
+    with open(output_file, 'w') as f:
+        json.dump(subset_data, f, indent=2)  # 'indent' for pretty formatting
+
 
 
 def read_database(db_path):
@@ -53,6 +67,42 @@ def read_with_re(def_nutrient, db_path):
     return result_df
 
 
+def chunk_foodb_content():
+    data_list = []
+
+    # Open the file and read it line by line
+    print('Reading the file line by line')
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            data = json.loads(line)
+            print(data)
+            data_list.append(data)
+
+    print('Converting to a pandas DF')
+    df = pd.DataFrame(data_list)
+    print(df.head())
+
+    # Set the maximum size for each chunk
+    max_rows_per_chunk = 500000
+
+    # Calculate the number of chunks needed
+    num_chunks = (len(df) // max_rows_per_chunk) + 1
+
+    # Specify the base name for the Excel files
+    base_excel_file_name = 'output_file_chunk'
+
+    # Export each chunk of the DataFrame to a separate Excel file
+    for chunk_num in range(num_chunks):
+        start_row = chunk_num * max_rows_per_chunk
+        end_row = (chunk_num + 1) * max_rows_per_chunk
+        df_chunk = df.iloc[start_row:end_row]
+
+        # Generate the Excel file name for the chunk
+        excel_file_path = f'{base_excel_file_name}_{chunk_num + 1}.xlsx'
+
+        # Export the chunk to Excel
+        df_chunk.to_excel(excel_file_path, index=False)
+
 def tackle_deficiency(def_nutrient: str, age: int, db_path: Path, test_method: str):
     """
     Run the whole deficiency tackler program. Given the deficiency, find the foods that one can have the nutrient
@@ -65,13 +115,13 @@ def tackle_deficiency(def_nutrient: str, age: int, db_path: Path, test_method: s
     tic = time()
     whole_db = pd.read_csv(db_path)
     nan_indices = whole_db[whole_db['source_id'].isna()].index
-    for i, nan_index in enumerate(nan_indices):
-        print(f'Working on index {i} of {len(nan_indices)}.')
-        row_text = whole_db.iloc[nan_index][0]
-        row_divided = next(csv.reader(io.StringIO(row_text), quotechar='"', delimiter=','))
-        converted_list = [float(x) if x.replace('.', '', 1).isdigit() else (np.nan if x == '' else x) for x in row_divided]  # convert list to right dtypes
-        df_parsed = pd.DataFrame(converted_list).T
-        whole_db.iloc[nan_index] = df_parsed.iloc[0]  # return df into original
+    # for i, nan_index in enumerate(nan_indices):
+    #     print(f'Working on index {i} of {len(nan_indices)}.')
+    #     row_text = whole_db.iloc[nan_index][0]
+    #     row_divided = next(csv.reader(io.StringIO(row_text), quotechar='"', delimiter=','))
+    #     converted_list = [float(x) if x.replace('.', '', 1).isdigit() else (np.nan if x == '' else x) for x in row_divided]  # convert list to right dtypes
+    #     df_parsed = pd.DataFrame(converted_list).T
+    #     whole_db.iloc[nan_index] = df_parsed.iloc[0]  # return df into original
     toc = time() - tic
     print(f'Duration for updating all database: {toc:.0f} seconds.')
     print('f')
@@ -94,12 +144,14 @@ def tackle_deficiency(def_nutrient: str, age: int, db_path: Path, test_method: s
 
 if __name__ == '__main__':
     repo_path = Path(repo_root_str_path)
-    database_path = repo_path / 'data/Content_unknown_removed.csv'
-    deficient_nutrient = 'luteolin'
-    age_in_years = 27
-    test_mode = 're'
-    tackle_deficiency(def_nutrient=deficient_nutrient, age=age_in_years, db_path=database_path,
-                      test_method=test_mode)
+    file_path = repo_path / 'data/Content.json'
 
 
 
+
+
+    print('Exporting pandas DF')
+    csv_file_path = repo_path / 'output_file.csv'
+    df.to_csv(csv_file_path, index=False)
+
+    whole_db = pd.read_csv(csv_file_path)
